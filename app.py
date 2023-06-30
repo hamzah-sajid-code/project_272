c8595b9603ba1c95dcc4d3bf0b5a1dc0df49dfd8
hamzah-sajid-code <82176013+hamzah-sajid-code@users.noreply.github.com>
2023-06-30 15:25:54 +0300

    menw

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def get_otp():
     if verification_check.status == "pending":
         return render_template('otp_error.html')
     else:
-        return redirect("https://project-c272.onrender.com/")
+        return redirect("https://google.com/")
 
 
 if __name__ == "__main__":
